25e018b5bd596b7defc75f8a85b087731cea9ead
skypse <skypse0136@gmail.com>
2024-08-23 19:04:47 -0300

    testando os tipos do python

---
+++ Tipos-de-dados/tipos-de-dados.py
@@ -0,0 +1,12 @@
+#Testando INT
+print("INT =",10 + 10 + 1000)
+
+#Testando FLOAT
+print("FLOAT =",1.5 + 1 + 0.5)
+
+#Testando BOOL
+print("Verdadeiro =",True)
+print("Falso =",False)
+
+#Testando STRING
+print("STRING = Olá Mundo!")
