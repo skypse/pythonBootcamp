2303e2f31731d6a3402126d497dc0ae50dc4f2f1
skypse <skypse0136@gmail.com>
2024-08-23 23:46:15 -0300

    adicionando exemplo-01-nao-existe-constantes.py

---
+++ Variaveis-e-Constantes/exemplo-01-nao-existe-constantes.py
@@ -0,0 +1,12 @@
+# Não existe uma palavra reservada para informar ao interpretador que o valor é constante.
+# Em algumas linguagens por exemplo: Java e C utilizamos 'final' e 'const', respectivamente para delcarar uma constante.
+# Python usamos a convenção (combinado) que diz ao programador que a variável é uma constante.
+# Para fazer isso você deve criar a variável com o nome em letras maíusculas:
+ABS_PATH = '/home/gabriel-do-amaral/PycharmProjects/pythonBootcamp'
+DEBUG = True
+STATES = [
+    'SP',
+    'RJ',
+    'SC'
+]
+AMOUNT = 30.2
