d8cc4f5ddd55cb15191b5ee36619a1cde690a6bf
skypse <skypse0136@gmail.com>
2024-08-23 23:58:44 -0300

    adicionando variaves_constantes-01.py

---
+++ Variaveis-e-Constantes/Praticando/variaves_constantes-01.py
@@ -0,0 +1,14 @@
+# Variáveis padronizadas
+item_brinqdo = "Brinquedo"
+cor_brinqdo = "Amarelo"
+quantidade_brinqdo = 1
+
+print(item_brinqdo, cor_brinqdo, quantidade_brinqdo)
+
+# Variável sugestivas
+qntd_max = 100
+
+# Constantes
+CORES_DE_BRINQUEDOS = ["AMARELO", "PRETO", "ROSA", "VERMELHO"]
+print(CORES_DE_BRINQUEDOS)
+
