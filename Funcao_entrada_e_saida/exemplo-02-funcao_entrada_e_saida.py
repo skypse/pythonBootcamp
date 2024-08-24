e3e27af795b209605b02eb8c7ce71d0b41bbbc96
skypse <skypse0136@gmail.com>
2024-08-24 00:58:49 -0300

    adicionando exemplo-02-funcao_entrada_e_saida.py

---
+++ Funcao_entrada_e_saida/exemplo-02-funcao_entrada_e_saida.py
@@ -0,0 +1,11 @@
+nome = "Gabriel"
+sobrenome = "Amaral"
+
+print(nome, sobrenome)
+# --> Gabriel Amaral
+
+print(nome, sobrenome, end="...\n")
+# --> Gabriel Amaral...
+
+print(nome, sobrenome, sep="#")
+# --> Gabriel#Amaral
