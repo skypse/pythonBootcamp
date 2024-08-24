e13813bff7816213ed982290b4bcc613386fb278
skypse <skypse0136@gmail.com>
2024-08-23 23:07:21 -0300

    adicionando exemplo-01-variavel.py

---
+++ Variaveis-e-Constantes/exemplo-01-variavel.py
@@ -0,0 +1,10 @@
+# Declarando uma por uma
+idade = 19
+nome = "Gabriel"
+print(f"Meu nome é {nome} e eu tenho {idade} ano(s) de idade.")
+# --> Meu nome é Gabriel e eu tenho 19 ano(s) de idade.
+
+# Várias variáveis na mesma linha
+idade, nome = (19, "Gabriel")
+print(f"Meu nome é {nome} e eu tenho {idade} ano(s) de idade.")
+# --> Meu nome é Gabriel e eu tenho 19 ano(s) de idade.
