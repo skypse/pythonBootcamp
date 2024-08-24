ef96f17ac50afc1f852298e092e85dd87625dc7b
skypse <skypse0136@gmail.com>
2024-08-24 00:33:16 -0300

    adicionando numerico_para_string.py

---
+++ Convertendo_tipos/numerico_para_string.py
@@ -0,0 +1,13 @@
+preco = 10.50
+produto_quantidade = 6
+produto_nome = "Salgadinho"
+
+print(str(preco))
+# --> 10.5
+
+print(str(produto_quantidade))
+# --> 6
+
+texto = f"Produto: {produto_nome}, custando {preco}. Restam {produto_quantidade} unidades"
+print(texto)
+# --> Produto: Salgadinho, custando 10.5. Restam 6 unidades
