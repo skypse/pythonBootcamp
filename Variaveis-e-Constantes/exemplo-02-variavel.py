ed08ba32df54817b65301569c6d30a340af165a8
skypse <skypse0136@gmail.com>
2024-08-23 23:16:55 -0300

    adicionando exemplo-02-variavel.py

---
+++ Variaveis-e-Constantes/exemplo-02-variavel.py
@@ -0,0 +1,17 @@
+# Declarando variáveis
+nome_pessoa = "Gabriel"
+carro = "Fusca"
+ano_carro = 2005
+
+# Utilizando valores padrões
+print(f"Oi eu sou o {nome_pessoa}, tenho um {carro} do ano {ano_carro}!")
+# --> Oi eu sou o Gabriel, tenho um Fusca do ano 2005!
+
+# Alterando valores
+nome_pessoa = "Amaral"
+carro = "BYD"
+ano_carro = 2024
+
+# Valores alterados
+print(f"Oi eu sou o {nome_pessoa}, tenho um {carro} do ano {ano_carro}!")
+# --> Oi eu sou o Amaral, tenho um BYD do ano 2024!
