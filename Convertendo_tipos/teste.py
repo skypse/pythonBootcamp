a3c0ab0b180357cfac8f8b3196c3b0e85663f538
skypse <skypse0136@gmail.com>
2024-08-24 00:44:26 -0300

    adicionando teste.py

---
+++ Convertendo_tipos/teste.py
@@ -0,0 +1,12 @@
+print(int(1.12652456))
+print(int("20"))
+print(float("20.10"))
+print(float(200))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor)) # mostrar a classe int
+print(type(valor_str)) # mostrar a classe str
+
+print(100 / 2) # trazer ele com float
+print(100 // 2) # trazer o número inteiro
