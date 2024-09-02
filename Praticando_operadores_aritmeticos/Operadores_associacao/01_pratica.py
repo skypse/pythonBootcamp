de2bf72aff5de8c01a267763d2e95a2d1c3fc549
skypse <skypse0136@gmail.com>
2024-09-02 19:09:01 -0300

    fazendo umas alteracoes

--- Praticando_operadores_aritmeticos/Operadores_associacao/01_pratica.py
+++ Praticando_operadores_aritmeticos/Operadores_associacao/01_pratica.py
@@ -5,9 +5,9 @@ saques = [1500, 100]
 # Tem "Python" em curso? SIM = true
 print("Python" in curso) # true
 
-# Tem "maça" em frutas? NÃO = TRUE
+# Tem "maça" em frutas? NÃO = false
 # Aqui retorna true, pois estamos utilizando o 'NOT IN'
-print("maça" not in frutas)
+print("maça" not in frutas) # false
 
-# Tem 200 em saques? SIM = true
-print(200 in saques) # True
+# Tem 200 em saques? NÃO = false
+print(200 in saques) # false
