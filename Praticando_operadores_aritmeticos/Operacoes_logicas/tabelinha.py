b41065328ebbd2f74909712a9687bd8c5a5cefde
skypse <skypse0136@gmail.com>
2024-09-02 18:40:47 -0300

    adicionando uma tabelinha.py, que contém exemplos de true or false...

---
+++ Praticando_operadores_aritmeticos/Operacoes_logicas/tabelinha.py
@@ -0,0 +1,11 @@
+# Tabela para consulta
+
+# AND = Para ser True, tudo tem que ser True
+print(True and True)
+print(True and False)
+print(False and False)
+
+# OR = Para ser True, apenas um tem que ser True
+print(True or True)
+print(True or False)
+print(False or False)
