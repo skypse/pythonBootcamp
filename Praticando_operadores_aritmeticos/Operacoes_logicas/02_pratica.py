7539496f54dff75f60dbe9aaedcbb990beb33d4b
skypse <skypse0136@gmail.com>
2024-09-02 14:51:24 -0300

    adicionando a 02_pratica.py

---
+++ Praticando_operadores_aritmeticos/Operacoes_logicas/02_pratica.py
@@ -0,0 +1,9 @@
+# Operação lógica com o 'OU' que é o 'OR'
+
+saldo = 1000
+saque = 200
+limite = 100
+
+# saldo é maior ou igual a saque? SIM! | limite é maior ou igual a saque? NÃO
+testando = saldo >= saque or saque <= limite # true!
+print(testando)
