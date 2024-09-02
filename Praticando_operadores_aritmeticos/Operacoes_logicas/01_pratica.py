6a7dad6f1fae2e0ccee9e6e5330093fabd5662f7
skypse <skypse0136@gmail.com>
2024-09-02 14:48:20 -0300

    adicionando a 01_pratica.py

---
+++ Praticando_operadores_aritmeticos/Operacoes_logicas/01_pratica.py
@@ -0,0 +1,9 @@
+# Operação lógica com o 'E' que é o 'and'
+
+saldo = 1000
+saque = 200
+limite = 100
+
+# saldo é maior ou igual ao saque? | saque é menor ou igual ao limite?
+testando = saldo >= saque and saque <= limite # false!
+print(testando)
