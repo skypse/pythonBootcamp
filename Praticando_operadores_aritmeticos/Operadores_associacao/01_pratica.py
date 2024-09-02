4b6ce6ead91f6b7b7fca840fffb4bb0a6c8d30a0
skypse <skypse0136@gmail.com>
2024-09-02 19:10:12 -0300

    fazendo umas alteracoes

--- Praticando_operadores_aritmeticos/Operadores_associacao/01_pratica.py
+++ Praticando_operadores_aritmeticos/Operadores_associacao/01_pratica.py
@@ -7,7 +7,7 @@ print("Python" in curso) # true
 
 # Tem "maça" em frutas? NÃO = false
 # Aqui retorna true, pois estamos utilizando o 'NOT IN'
-print("maça" not in frutas) # false
+print("maça" not in frutas) # true
 
 # Tem 200 em saques? NÃO = false
 print(200 in saques) # false
